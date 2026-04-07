b33f78797f31a13970c6a632e299c4339411f36d
Anupam Shrivastava <anupamshri1906@gmail.com>
2026-04-07 15:52:52 +0530

    Delete regret-predictor directory

--- regret-predictor/server/routes/predictionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPrediction } = require('../controllers/predictionController');
-const { protect } = require('../middleware/auth');
-
-// POST /api/predict (protected)
-router.post('/', protect, getPrediction);
-
-module.exports = router;
